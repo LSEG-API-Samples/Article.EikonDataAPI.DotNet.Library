19a62b44dfce2ef8ac22efd0a4115eba27b7cac5
Jirapongse Phuriphanvichai <jirapongse.phuriphanvichai@refinitiv.com>
2021-04-27 14:35:03 +0700

    Add files via upload

---
+++ EikonDataAPI/EikonDataAPI_46/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EikonDataAPI")]
+[assembly: AssemblyDescription("EikonDataAPI (64bit) for .NET Framework 4.6")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Refinitiv")]
+[assembly: AssemblyProduct("EikonDataAPI")]
+[assembly: AssemblyCopyright("Copyright © Refinitiv 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33e2f447-a72a-4934-bd4e-5c422f3c2be9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.4.5")]
+[assembly: AssemblyFileVersion("2.0.4.5")]
